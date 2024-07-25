d8cfad0aaef58bd26f7ce11997da1c3663bbfb6d
unknown <mo.mahrous80@gmail.com>
2024-07-25 08:08:44 +0300

    start with login screen and finish welcome email password

---
+++ lib/screens/signin.dart
@@ -0,0 +1,72 @@
+import 'package:fashions/styles/icons.dart';
+import 'package:flutter/material.dart';
+
+class Signin extends StatelessWidget {
+  const Signin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(20),
+        height: MediaQuery.sizeOf(context).height,
+        width: MediaQuery.sizeOf(context).width,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 50, top: 50),
+              child: Image.asset(appLogo),
+            ),
+            Container(
+              width: MediaQuery.sizeOf(context).width,
+              child: Column(
+                children: [
+                  Text(
+                    "Welcome!",
+                    style: TextStyle(
+                        fontSize: 26,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
+                  ),
+                  Text(
+                    "please login or sign up to continue our app",
+                    style: TextStyle(fontSize: 16, color: Colors.grey[500]),
+                  )
+                ],
+                crossAxisAlignment: CrossAxisAlignment.start,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 50),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  labelStyle: TextStyle(
+                      color: Colors.black,
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold),
+                  floatingLabelBehavior: FloatingLabelBehavior.auto,
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  labelStyle: TextStyle(
+                      color: Colors.black,
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold),
+                  floatingLabelBehavior: FloatingLabelBehavior.auto,
+                  border: InputBorder.none,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
